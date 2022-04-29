703e0d930747fbe9fbaa9504fa506da179fa3aaf
Galprim <galprimulus@gmail.com>
2022-04-30 01:28:33 +0300

    init commit

---
+++ Task29/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из некоторого количества элементов 
+//и выводит их на экран с помощью функций. 
+//(можно спрашивать количество вводимых элементов и вводить все значения с клавиатуры)
+
+Console.Write("Количество элементов: ");
+int index = Convert.ToInt32(Console.ReadLine());
+
+int[] Numbers (int index)
+{
+    int[] thisArray = new int[index];
+    int i = 0;
+    while(i < index)
+    {
+        Console.Write($"элемент {i+1}: ");
+        int arrayNumber = Convert.ToInt32(Console.ReadLine());
+        thisArray[i] = arrayNumber;
+        i++;
+    }
+    return thisArray;
+}
+
+void PrintArray (int[] array, int index)
+{
+    for (int i = 0; i < index; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] array = Numbers(index);
+Console.WriteLine("Получился вот такой массив: ");
+PrintArray(array, index);
