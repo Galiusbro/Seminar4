6ca15b22ecd26ed68fd4effba8c538ba96bb578f
Galprim <galprimulus@gmail.com>
2022-04-30 02:02:39 +0300

    add tasks 27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 27: Напишите функцию, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void Sum (int number)
+{
+    if (number < 0)
+    {
+        number = -number;
+    }
+    int sum = 0;
+    while (number > 0)
+    {
+    sum = sum + (number % 10);
+    number /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Sum(num);
