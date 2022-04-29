23b268c55d69a488ec7d9ddc260c44a70817d97f
Galprim <galprimulus@gmail.com>
2022-04-30 01:42:13 +0300

    add tasks 25 and 29

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 25: Напишите (функцию) цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void Exponentiate (int a, int b)
+{
+    int exp = a;
+    for (int i = 1; i < b; i++)
+    {
+        exp = exp * a;
+    }
+    Console.WriteLine($"{a}^{b} = {exp}");
+}
+
+Console.Write("Я хочу число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Возвести в степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+Exponentiate(number, degree);
